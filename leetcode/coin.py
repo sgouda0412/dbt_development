d30da38da2da474521cc939bf32e6a2cbd4cb499
sgouda0412 <sgouda0412@gmail.com>
2025-08-06 10:48:04 +0530

    good to go

---
+++ leetcode/coin.py
@@ -0,0 +1,13 @@
+def coin_change(amount: int, coins: list[int]) -> int:
+    pass
+
+
+def main():
+    amount = 11
+    coins = [1, 2, 5]
+    res = coin_change(amount, coins)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
