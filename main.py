b646aaf8db34310971d4f68f06de9971b14a93d7
sgouda0412 <sgouda0412@gmail.com>
2025-08-06 10:50:00 +0530

    goog tpp go

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello World from Python Programming!!!!")
+
+
+def fuck(x, y):
+    return x, y
+
+
+def func(x):
+    return x
+
+
+if __name__ == "__main__":
+    print(func(12))
+print(fuck(10, 12))
